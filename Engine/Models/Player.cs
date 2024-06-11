4a8dc625878fb922a7d157dc484b7dd90c25c5e8
ryan1604 <ryanchua1604@gmail.com>
2024-06-11 18:07:18 +0800

    Initial setup

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Player
+    {
+        public string name { get; set; }
+        public string characterClass { get; set; }
+        public int hitPoints { get; set; }
+        public int experiencePoints { get; set; }
+        public int level { get; set; }
+        public int gold { get; set; }
+    }
+}
---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,21 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    internal class GameSession
+    {
+        Player currentPlayer { get; set; }
+
+        public GameSession()
+        {
+            currentPlayer = new Player();
+            currentPlayer.name = "Ryan";
+            currentPlayer.gold = 100000;
+        }
+    }
+}
