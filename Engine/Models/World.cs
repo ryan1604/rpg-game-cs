b1b67e80882ae28f6b06ef489fca7c285fe318be
ryan1604 <ryanchua1604@gmail.com>
2024-06-12 11:46:47 +0800

    Create world and add locations

---
+++ Engine/Models/World.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class World
+    {
+        private List<Location> _locations = new List<Location>();
+
+        internal void AddLocation(int xCoord, int yCoord, string name, string desc, string imageName)
+        {
+            Location loc = new Location();
+            loc.XCoordinate = xCoord;
+            loc.YCoordinate = yCoord;
+            loc.Name = name;
+            loc.Description = desc;
+            loc.ImageName = imageName;
+
+            _locations.Add(loc);
+        }
+
+        public Location LocationAt(int xCoord, int yCoord)
+        {
+            foreach (Location loc in _locations)
+            {
+                if (loc.XCoordinate == xCoord && loc.YCoordinate == yCoord)
+                {
+                    return loc;
+                }
+            }
+            return null;
+        }
+    }
+}
