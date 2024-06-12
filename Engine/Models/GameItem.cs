cd96a422002e00be4c4ca483e009a80df0c0eac9
ryan1604 <ryanchua1604@gmail.com>
2024-06-12 23:51:25 +0800

    Add create items function

---
+++ Engine/Models/GameItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class GameItem(int itemTypeID, string name, int price)
+    {
+        public int ItemTypeID { get; set; } = itemTypeID;
+        public string Name { get; set; } = name;
+        public int Price { get; set; } = price;
+
+        public GameItem Clone()
+        {
+            return new GameItem(ItemTypeID, Name, Price);
+        }
+    }
+}
---
+++ Engine/Models/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Weapon(int itemTypeID, string name, int price, int minDamage, int maxDamage) 
+        : GameItem(itemTypeID, name, price)
+    {
+        public int MinimumDamage { get; set; } = minDamage;
+        public int MaximumDamage { get; set; } = maxDamage;
+
+        public new Weapon Clone()
+        {
+            return new Weapon(ItemTypeID, Name, Price, MinimumDamage, MaximumDamage);
+        }
+    }
+}
