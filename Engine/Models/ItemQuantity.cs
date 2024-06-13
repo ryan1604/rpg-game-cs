5477f8c041718242e5bce585934d3abedb3cab7c
ryan1604 <ryanchua1604@gmail.com>
2024-06-13 15:08:43 +0800

    Add quests

---
+++ Engine/Models/ItemQuantity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class ItemQuantity(int itemID, int quantity)
+    {
+        public int ItemID { get; set; } = itemID;
+        public int Quantity { get; set; } = quantity;
+    }
+}
