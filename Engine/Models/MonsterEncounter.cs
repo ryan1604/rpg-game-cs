f7ac73ec1fa3b6b014f222ed518b3a908175d54b
ryan1604 <ryanchua1604@gmail.com>
2024-06-13 22:49:06 +0800

    Add monster to location

---
+++ Engine/Models/MonsterEncounter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class MonsterEncounter(int monsterID, int chanceOfEncountering)
+    {
+        public int MonsterID { get; set; } = monsterID;
+        public int ChanceOfEncountering { get; set; } = chanceOfEncountering;
+    }
+}
