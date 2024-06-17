d802a933b9616f376b302dc5855c8cf70e6d849e
ryan1604 <ryanchua1604@gmail.com>
2024-06-17 16:15:11 +0800

    Refine GameItem class

--- Engine/Models/GameItem.cs
+++ Engine/Models/GameItem.cs
@@ -6,16 +6,26 @@ using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    public class GameItem(int itemTypeID, string name, int price, bool isUnique = false)
+    public class GameItem(GameItem.ItemCategory category, int itemTypeID, string name, int price, 
+        bool isUnique = false, int minDamage = 0, int maxDamage = 0)
     {
+        public enum ItemCategory
+        {
+            Miscellaneous,
+            Weapon
+        }
+
+        public ItemCategory Category { get; } = category;
         public int ItemTypeID { get; } = itemTypeID;
         public string Name { get; } = name;
         public int Price { get; } = price;
         public bool IsUnique { get; } = isUnique;
+        public int MinimumDamage { get; } = minDamage;
+        public int MaximumDamage { get; } = maxDamage;
 
         public GameItem Clone()
         {
-            return new GameItem(ItemTypeID, Name, Price, IsUnique);
+            return new GameItem(Category, ItemTypeID, Name, Price, IsUnique, MinimumDamage, MaximumDamage);
         }
     }
 }
--- Engine/Models/LivingEntity.cs
+++ Engine/Models/LivingEntity.cs
@@ -68,7 +68,7 @@ namespace Engine.Models
         public ObservableCollection<GameItem> Inventory { get; }
 
         public ObservableCollection<GroupedInventoryItem> GroupedInventory { get; }
-        public List<GameItem> Weapons => Inventory.Where(i => i is Weapon).ToList();
+        public List<GameItem> Weapons => Inventory.Where(i => i.Category == GameItem.ItemCategory.Weapon).ToList();
 
         public bool IsDead => CurrentHitPoints <= 0;
 
